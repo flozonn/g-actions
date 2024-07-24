ab2f07f2b3e61cf93ef02f1cef87625b0261e110
flozonn <florian.breton1@icloud.com>
2024-07-24 15:10:33 +0200

    Create New server and new S3 bucket

--- s3.tf
+++ s3.tf
@@ -1,6 +1,15 @@
 
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "bucket-de-logs"
+resource "aws_s3_bucket" "log_buckeet" {
+  bucket = "nouveau bucket"
+}
+
+// ec2 instance linux
+resource "aws_instance" "application_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Mon_server_F2I_DSP"
+  }
 }
 
