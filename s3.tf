ad51ad868c9d63c73aa5f36dad2c4bcb24b72de0
flozonn <florian.breton1@icloud.com>
2024-07-24 15:13:25 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,12 +1,12 @@
 
 
 resource "aws_s3_bucket" "log_buckeet" {
-  bucket = "nouveau bucket"
+  bucket = "nouveau_bucket"
 }
 
 // ec2 instance linux
 resource "aws_instance" "application_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-09042b2f6d07d164a"
   instance_type = "t2.micro"
   tags = {
     Name = "Mon_server_F2I_DSP"
